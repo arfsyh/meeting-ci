e1d6254f6846cc2b67af02273c9d264c49fee8e4
sayaarfiansyah <arfyan.s44@gmail.com>
2020-10-26 18:37:29 +0700

    third_party

--- application/controllers/CreateEvent.php
+++ application/controllers/CreateEvent.php
@@ -1,5 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set('Asia/Jakarta');
+set_include_path(APPPATH . 'third_party/' . PATH_SEPARATOR .     get_include_path());
+require_once APPPATH . 'third_party/vendor/autoload.php';
 
 
 class CreateEvent extends CI_Controller {
@@ -47,13 +49,14 @@ class CreateEvent extends CI_Controller {
 	}
 	function getClient()
 	{
-		$this->load->add_package_path(APPPATH . './assets/vendor/autoload');
+		
 
 
 		$client = new Google_Client();
 		$client->setApplicationName('Google Calendar API PHP Quickstart');
 		$client->setScopes(Google_Service_Calendar::CALENDAR);
 		$credentials = file_get_contents("./assets/data/credentials.json");
+		echo $credentials;
 		$client->setAuthConfig($credentials);
 		$client->setAccessType('offline');
 		$client->setPrompt('select_account consent');
@@ -70,16 +73,29 @@ class CreateEvent extends CI_Controller {
 			if ($client->getRefreshToken()) {
 				$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
 			} else {
+  
+				//pindahkan ke halaman ambil token sambil bawa session
+				//header("location:tes_oauth.php");
+				$_SESSION['event_group'] = 'Contoh';
+				$_SESSION['event_title'] = 'contoh ci';
+				$_SESSION['event_place'] = 'jogja';
+				$_SESSION['event_start_time'] = $_POST['event-start-time'];
+				$_SESSION['event_end_time'] = $_POST['event-end-time'];
+				$_SESSION['event_description'] = $_POST['event-description'];
+				
 				// Request authorization from the user.
-				$authUrl = $client->createAuthUrl();
+				//$authUrl = $client->createAuthUrl();
+				$_SESSION['authURL'] = $client->createAuthUrl();
+				/*
 				printf("Open the following link in your browser:\n%s\n", $authUrl);
 				print 'Enter verification code: ';
-				$authCode = trim(fgets(STDIN));
-	
+				$authCode = trim(fgets(STDIN)); */
+  
 				// Exchange authorization code for an access token.
-				$accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+			   
+				$accessToken = $client->fetchAccessTokenWithAuthCode($_SESSION['authCode']);
 				$client->setAccessToken($accessToken);
-	
+  
 				// Check to see if there was an error.
 				if (array_key_exists('error', $accessToken)) {
 					throw new Exception(join(', ', $accessToken));
@@ -92,6 +108,39 @@ class CreateEvent extends CI_Controller {
 			file_put_contents($tokenPath, json_encode($client->getAccessToken()));
 		}
 		return $client;
+	}
+
+	public function Create(){
+		$client = $this->getClient();
+  	$service = new Google_Service_Calendar($client);
+
+  	$data = array(
+    'summary' => 'contoh',
+    'location' => 'jogja',
+    'sendNotifications' => TRUE,
+    'sendUpdates' => TRUE,
+    'description' => '',
+    'start' => array(
+      'dateTime' => '2020-11-01T22:00:00+07:00',
+      'timeZone' => 'Asia/Jakarta',
+    ),
+    'end' => array(
+      'dateTime' => '2020-11-01T22:00:00+07:00',
+      'timeZone' => 'Asia/Jakarta',
+    ),
+    'attendees' => 'arfyan.vira@gmail.com',
+    'reminders' => array(
+      'useDefault' => FALSE,
+      'overrides' => array(
+        array('method' => 'email', 'minutes' => 24 * 60),
+        array('method' => 'popup', 'minutes' => 10),
+      ),
+    ),
+  );
+
+  	$event = new Google_Service_Calendar_Event($data);
+	$calendarId = 'primary';
+	$event = $service->events->insert($calendarId, $event);
 	}
 
 }
